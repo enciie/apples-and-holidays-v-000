bc6a77bf552a8eddf013ca3cab9784c15744a804
Nancy Jo <enciie@gmail.com>
2018-05-03 22:41:24 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -91,7 +91,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holiday|
     holiday.map do |holiday_name, supplies|
       if supplies.include?("BBQ")
-        holiday_name
+        holiday_name.flatten
       end
     end
   end
